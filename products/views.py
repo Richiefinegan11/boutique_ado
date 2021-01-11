c4950f6d61c3d4f3d6cc24452d890cd2d5937653
Richiefinegan11 <richiefinegan1990@gmail.com>
2021-01-11 20:15:11 +0000

    added sorting counts to product page

--- products/views.py
+++ products/views.py
@@ -20,9 +20,10 @@ def all_products(request):
             sortkey = request.GET['sort']
             sort = sortkey
             if sortkey == 'name':
-                sortkey == 'lower_name'
+                sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
@@ -37,7 +38,7 @@ def all_products(request):
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You didn't enter any search criteria")
+                messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
 
             queries = Q(name__icontains=query) | Q(description__icontains=query)
@@ -49,7 +50,7 @@ def all_products(request):
         'products': products,
         'search_term': query,
         'current_categories': categories,
-        'curent_sorting': current_sorting
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)

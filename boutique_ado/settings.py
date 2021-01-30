86fee6523b5d4c939bfb1b4fe5d4bf55291fd569
Richiefinegan11 <richiefinegan1990@gmail.com>
2021-01-30 09:13:08 +0000

    055 - deployment part 5

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -177,17 +177,17 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # AWS
 if 'USE_AWS' in os.environ:
-    # bucket config
+    # Bucket config
     AWS_STORAGE_BUCKET_NAME = 'richie-finegan-boutique-ado'
-    AWS_S3_REGION_NAME = 'user-east-1'
+    AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_ACCESS_KEY_ID')
-    AWS_S3_CUSTOM_DOMAIN = F'{AWS_STORAGE_BUCKET_NAME}'.s3.amazonaws.com
+    AWS_S3_CUSTOM_DOMAIN = F'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
-    DEFAUL_FILE_STORAGE = 'custom_storages.MediaStorage'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
     # Override static and media URLs in production

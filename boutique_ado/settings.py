2097d8a79ddd2c99f22543a431d04a051ad4dc41
Richiefinegan11 <richiefinegan1990@gmail.com>
2021-02-02 18:08:43 +0000

    Shopping bag refactor

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -11,8 +11,6 @@ import os
 import dj_database_url
 
 
-STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -186,7 +184,7 @@ if 'USE_AWS' in os.environ:
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = F'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
